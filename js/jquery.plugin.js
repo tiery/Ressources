5989b28e897729aadb9013db930a264fc430ba96
Tiery <tierymail@gmail.com>
2012-06-15 16:44:12 +0300

    Update master

--- js/jquery.plugin.js
+++ js/jquery.plugin.js
@@ -8,11 +8,25 @@
         defaults = {};
 
     // Plugin constructor
-    var Plugin = function (element, options) {
-        var that = this;
-        that.$element = $(element);
-        that.opts = $.extend({}, defaults, options);
-        that.init();
+    var Plugin = function (element, options, i) {
+        
+        // Plugin exposed to window
+        window[pluginName + i] = this;
+
+        // DOM elements
+        this.$element = $(element).attr('data-plugin-id', pluginName + i);
+
+        // Relate slider plugin
+        // this.Slider = window[this.$element.find('[data-slider="element"]').attr('data-plugin-id')];
+
+        // Properties
+        // this.id    = this.$element.attr('data-slideshow-id');
+
+        // Defaults -> options
+        this.opts = $.extend({}, defaults, options);
+
+        // Go!
+        this.init();
     };
     
     // Shortcut for Plugin object prototype
@@ -21,16 +35,17 @@
     // Initialization logic
     Plugin.fn.init = function () {
         var that = this;
-        // DOM Elements
-        // Element specific variables
+        
+        // Events init
+        that.events();
     };
 
     // Plugin wrapper around the constructor,
     // preventing against multiple instantiations
     $.fn[pluginName] = function (options) {
-        return this.each(function () {
+        return this.each(function (i) {
             if (!$.data(this, 'plugin_' + pluginName)) {
-                $.data(this, 'plugin_' + pluginName, new Plugin(this, options));
+                $.data(this, 'plugin_' + pluginName, new Plugin(this, options, i));
             }
         });
     };
