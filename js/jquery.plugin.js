33d82a7d4d6f613f798975cd6ed400db38df8ed7
Tiery <tierymail@gmail.com>
2012-10-11 12:22:52 +0300

    Update js/jquery.plugin.js

--- js/jquery.plugin.js
+++ js/jquery.plugin.js
@@ -1,52 +1,57 @@
 /*
- * [PLUGIN NAME]
- * Provide a quick description of this jQuery plugin
+ * [plugin_name + description]
  */
 (function ($) {
 
-	var pluginName = '[PLUGIN NAME]',
-		defaults = {};
+    'use strict';
 
-	// Plugin constructor
-	var Plugin = function (element, options, i) {
-		
-		// Plugin exposed to window
-		window[pluginName + i] = this;
+    // Params
+    var pluginName = '[plugin_name]',
+        defaults = {};
+        
+    // Plugin constructor
+    var Plugin = function (element, options, i) {
+        
+        // Plugin exposed to window
+        
+        // DOM elements
+        this.$element = $(element).attr('data-' + pluginName + '-id', pluginName + i);
+                
+        // Properties
+        this.[property_name] = [property_value];
+        
+        // Options
+        var opts  = $.extend({}, options, this.$element.data(pluginName + '-options')); // element options -> plugin init options
+        this.opts = $.extend({}, defaults, opts); // options -> default
+        
+        // Go!
+        this.init();
+    };
+    
+    // Shortcut for Plugin object prototype
+    Plugin.fn = Plugin.prototype;
+    
+    // Initialization logic
+    Plugin.fn.init = function () {
+        var that = this;
+        
+        // Events init
+        that.events();
+    };
+    
+    // Evenements
+    Plugin.fn.events = function () {
+        var that = this;
+    };
+    
+    // Plugin wrapper around the constructor,
+    // preventing against multiple instantiations
+    $.fn[pluginName] = function (options) {
+        return this.each(function (i) {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName, new Plugin(this, options, i));
+            }
+        });
+    };
 
-		// DOM elements
-		this.$element = $(element).attr('data-plugin-id', pluginName + i);
-
-		// Related plugin
-		// this.Slider = window[this.$element.find('[data-slider="element"]').attr('data-plugin-id')];
-
-		// Properties
-		// this.id = this.$element.attr('data-slideshow-id');
-
-		// Defaults -> options
-		var elOpts = this.$element.data('[PLUGIN NAME]-options') || {};
-		var opts   = $.extend({}, options, elOpts); // element options -> (plugin init) options
-		this.opts  = $.extend({}, defaults, opts); // options -> default
-
-		// Go!
-		this.init();
-	};
-	
-	// Shortcut for Plugin object prototype
-	Plugin.fn = Plugin.prototype;
-	
-	// Initialization logic
-	Plugin.fn.init = function () {
-		var that = this;
-	};
-
-	// Plugin wrapper around the constructor,
-	// preventing against multiple instantiations
-	$.fn[pluginName] = function (options) {
-		return this.each(function (i) {
-			if (!$.data(this, 'plugin_' + pluginName)) {
-				$.data(this, 'plugin_' + pluginName, new Plugin(this, options, i));
-			}
-		});
-	};
-
-})(jQuery);
+}(jQuery));
